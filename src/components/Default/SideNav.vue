a73b208ce8c56c1fca47e105003c734c605841d6
Shana Maggie <maggieyang0809@gmail.com>
2024-03-12 13:57:55 +0800

    update

--- src/components/Default/SideNav.vue
+++ src/components/Default/SideNav.vue
@@ -15,16 +15,6 @@
 
 <script setup>
 const Categories = [
-  { name: "商業管理", route: "/course-view" },
-  { name: "國際交流", route: "/course-view/international" },
-  { name: "專業領導", route: "null" },
-  { name: "教育發展", route: "null" },
-  { name: "文化創意", route: "null" },
-  { name: "社會科學", route: "null" },
-  { name: "科技應用", route: "null" },
-  { name: "語言文學", route: "/course-view/language" },
-  { name: "資訊應用", route: "null" },
-];
     {name: '商業管理', route: '/course-view'},
     {name: '國際交流', route: '/course-view/international'},
     {name: '專業領導', route: '/course-view'},
