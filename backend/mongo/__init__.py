2e4e4770a69ae8dc4236ac1d63e7f49108d5578e
伍欣宜 <wuxinyi@xinyis-MacBook-Pro.local>
2024-03-31 20:05:15 +0800

    login(localstorage success)

--- backend/mongo/__init__.py
+++ backend/mongo/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import pymongo
 from pymongo import MongoClient
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+jwt = JWTManager(app)
 app.config["SECRET_KEY"] = "490db5992f28fa9f572613d6d53f61e7d15b391b"
+app.config["JWT_SECRET_KEY"] = "74df6a1e89ca4ad077ab2b0ed7076b2a808f8000e6fad4f6f5bb44eaed8c8d19"
 client = MongoClient("mongodb+srv://emily911008:emily450150@testing.mnvj9qd.mongodb.net/?retryWrites=true&w=majority")
 db = client["test"]
+jwt.init_app(app)
 
 from .school import school
 from .auth import auth
--- backend/mongo/auth.py
+++ backend/mongo/auth.py
@@ -1,25 +1,33 @@
-from flask import Blueprint, request, jsonify, flash, session
-from flask_cors import CORS
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import (create_access_token, create_refresh_token, 
+                                set_access_cookies, set_refresh_cookies, 
+                                jwt_required, get_jwt_identity, unset_jwt_cookies)
 from mongo import db
 from datetime import datetime
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods = ['POST'])
+@auth.route('/login', methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        response_object = {'status':'success'}
         user = request.get_json()
         mail = user.get('email')
         password = user.get('password')
         collection = db["user"]
-        user = collection.find_one({"email": mail})
-        if user:
-            check = user['password']
+        u = collection.find_one({"email": mail})
+        if u:
+            check = u['password']
             if password == check:
-                session["email"] = mail
-                response_object['message'] = 'success.'
-                return jsonify(response_object)
+                access_token = create_access_token(identity=mail)
+                refresh_token = create_refresh_token(identity=mail)
+                #response = jsonify()
+                #set_access_cookies(response, access_token)
+                #set_refresh_cookies(response, refresh_token)
+                #print(access_token)
+                return jsonify(access_token=access_token), 200
+        
+    else:
+            return jsonify({'error': 'error'})
 
 @auth.route('/signup', methods = ["GET", 'POST'])
 def sign_up():
@@ -32,11 +40,13 @@ def sign_up():
         school = new_user.get('school')
         department = new_user.get('department')
         dt = datetime.now()
+        role = 1
         collection = db["user"]
         collection.insert_one({
             "email" : mail,
             "user_name" : name,
             "password" : password,
+            "role" : role,
             "school" : school,
             "department" : department,
             "datetime" : dt
@@ -44,3 +54,20 @@ def sign_up():
         response_object['message'] = 'success.'
         return jsonify(response_object)
     return
+
+@auth.route('/userinfo', methods = ['GET'])
+@jwt_required()
+def userinfo():
+    print('success')
+    email = get_jwt_identity()
+    collection = db["user"]
+    user = collection.find_one({"email": email})
+    name = user['name']
+    university = user['school']
+    major = user['department']
+    if(user['role'] == 1):
+        role = '學生'
+    else:
+        role = '管理者'
+    user_data = {'name': name, 'email': email, 'university': university, 'major': major, 'iden': role}
+    return jsonify(user_data)
