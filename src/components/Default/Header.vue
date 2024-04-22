97a877c46ab0ed135d2ba3536f6c2f7dae932082
伍欣宜 <wuxinyi@xinyis-MacBook-Pro.local>
2024-04-22 21:32:08 +0800

    header

--- src/components/Default/Header.vue
+++ src/components/Default/Header.vue
@@ -35,15 +35,15 @@
 
     <v-spacer />
 
-    <v-btn to="/admin" variant="text" height="100%"> 管理者介面 </v-btn>
+    <v-btn to="/admin" variant="text" height="100%" v-if="headerfile.iden == 0"> 管理者介面 </v-btn>
 
     <v-btn to="" variant="text" height="100%"> 操作手冊 </v-btn>
     <v-btn to="/course-view/business" variant="text" height="100%"> 學分學程 </v-btn>
 
-    <v-menu open-on-hover>
+    <v-menu open-on-hover v-if="isloggedin">
       <template v-slot:activator="{ props }">
         <v-btn to="/profile" v-bind="props" variant="text" height="100%">
-          {{ userName }}
+          {{ headerfile.name }}
         </v-btn>
       </template>
 
@@ -55,9 +55,9 @@
       </v-list>
     </v-menu>
 
-    <v-btn to="/login" class="mr-5" variant="text" height="100%"> 登入 </v-btn>
+    <v-btn to="/login" class="mr-5" variant="text" height="100%" v-if="!isloggedin "> 登入 </v-btn>
 
-    <v-btn to="/" class="mr-5" variant="text" height="100%"> 登出 </v-btn>
+    <v-btn to="/" class="mr-5" variant="text" height="100%" v-if="isloggedin" @click = "logout"> 登出 </v-btn>
     <!-- // -->
 
     <!--v-btn to="/profile" variant="text" height="100%">
@@ -74,7 +74,7 @@
 
 <script setup>
 import { ref } from 'vue';
-import axios from 'axios';
+
 
 const userAvatar = ref('url_to_default_avatar'); // 替換為用戶頭像的 URL
 const userName = ref(''); // 初始使用者名稱設為空字串
@@ -86,7 +86,7 @@ const profile = [
   { name: "新增課程", route: "/profile/uploadcourse" },
 ];
 
-const header = () => {
+/*const header = () => {
   const user = JSON.parse(localStorage.getItem('user'));
   if(user && user["access_token"]){
     return {Authorization : `Bearer ${user["access_token"]}`};
@@ -96,24 +96,87 @@ const header = () => {
   }
 };
 
-const fetchUserInfo = () => {
+const fetchUserInfo = async() => {
   // 假設這是從後端 API 獲取的用戶信息
+  if(localStorage.getItem('user')){
   const userInfo = {
     avatar: 'url_to_user_avatar',
     name: '', // 假設使用者名稱從後端 API 獲取為 "Amy"
     role: '',
   };
 
+  const path = 'http://127.0.0.1:5000/userinfo';
+  try{
+    const response = await axios.get(path, { headers: header() });
+    userInfo.name = response.data.name;
+    userInfo.role = response.data.iden;
+    userAvatar.value = userInfo.avatar;
+    userName.value = userInfo.name;
+    userRole = userInfo.role;
+  }catch(error){
+      console.error(error);
+  };
   // 更新用戶頭像和名稱
-  userAvatar.value = userInfo.avatar;
-  userName.value = userInfo.name;
+  
+  }
 };
 
 // 在組件初始化時獲取用戶信息
-fetchUserInfo();
+fetchUserInfo();*/
+</script>
 
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      headerfile: {
+        name: '',
+        iden: '',
+      },
+      isloggedin: false,
+    }
+  },
 
-
+  methods: {
+    header() {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (user && user["access_token"]) {
+        return { Authorization: `Bearer ${user["access_token"]}` };
+      }
+      else {
+        return {};
+      }
+    },
+    getinfo() {
+      const path = 'http://127.0.0.1:5000/userinfo';
+      axios.get(path, { headers: this.header() })
+        .then((res) => {
+          this.headerfile.name = res.data.name;
+          this.headerfile.iden = res.data.iden;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    logout() {
+      localStorage.removeItem('user');
+      if (localStorage.getItem('courseList')) {
+        localStorage.removeItem('courseList')
+      }
+      router.push('/');//不知為何不會換頁面
+    }
+  },
+  created() {
+    if (localStorage.getItem('user')) {
+      this.isloggedin = true;
+      this.getinfo();
+    }
+    else{
+      this.isloggedin = false;
+    }
+  },
+}
 </script>
 
 
